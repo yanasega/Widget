67a2e2b2298a681eb1e78a3c8c7e648e1b2839f8
Yana Segal <yana.segal@intel.com>
2017-05-13 18:03:51 +0300

    all good

--- scripts.js
+++ scripts.js
@@ -1,17 +1,22 @@
 
 var move = "-100px";
-var sliderLimit = -750;
+var sliderLimit = -80;
 
 function moveright(){
 	var element = document.getElementById('tslshow');
     style = window.getComputedStyle(element);
 	var currentPosition = parseInt(style.getPropertyValue('left'));
     if (currentPosition >= sliderLimit){
+		document.getElementById("leftArrow").style.display = "block";
 		if(element.style.left){
 			element.style.left = String(parseInt(element.style.left) - 100) + "px";
+			if (currentPosition >= sliderLimit){
+				document.getElementById("rightArrow").style.display = "none";
+			}
 		}
 		else{
 			element.style.left = move;
+			document.getElementById("rightArrow").style.display = "none";
 		}
 	} 
 }
@@ -21,8 +26,19 @@ function moveleft(){
     style = window.getComputedStyle(element);
 	var currentPosition = parseInt(style.getPropertyValue('left'));
     if (currentPosition < 0){
+		document.getElementById("rightArrow").style.display = "block";
 		if(element.style.left){			
 			element.style.left = String(parseInt(element.style.left) +100) + "px";
 		}
+		if (currentPosition < 0){
+			document.getElementById("leftArrow").style.display = "none";
+		}
 	} 
+	else{
+		document.getElementById("leftArrow").style.display = "none";
+	}
+}
+
+window.onload = function(){
+	document.getElementById("leftArrow").style.display = "none";
 }
